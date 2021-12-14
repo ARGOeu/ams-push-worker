b8d027df125473a3b9d04f05c76ea7cd116ef54c
Konstantinos Kagkelidis <kaggis@gmail.com>
2021-12-14 10:23:27 +0200

    Make cli params more specific. Change pull interval in ms. Add default pull interval value to 300ms. Update Readme

--- main.go
+++ main.go
@@ -3,22 +3,34 @@ package main
 import (
 	"crypto/tls"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
 
-	var project = flag.String("project", "", "AMS project")
-	var sub = flag.String("sub", "", "AMS subscription")
-	var host = flag.String("host", "", "AMS host")
-	var token = flag.String("token", "", "AMS token")
-	var pollRate = flag.Int64("poll", 0, "Poll rate")
-	var remoteEndpoint = flag.String("endpoint", "", "Remote endpoint url")
-	var remoteEndpointAuthHeader = flag.String("auth", "", "Remote endpoint expected authorization header")
+	var project = flag.String("ams-project", "", "AMS project to be used (required)")
+	var sub = flag.String("ams-sub", "", "AMS subscription to pull messages from (required)")
+	var host = flag.String("ams-host", "", "AMS host and port (host:port) (required)")
+	var token = flag.String("ams-token", "", "AMS access token (required)")
+	var pollRate = flag.Int64("pull-interval", 300, "Interval in milliseconds between pulling the next message")
+	var remoteEndpoint = flag.String("remote-endpoint", "", "Remote endpoint url to push messages to (required)")
+	var remoteEndpointAuthHeader = flag.String("auth-header", "", "Remote endpoint expected authorization header")
 
 	flag.Parse()
+
+	requiredParams := []string{"ams-host", "ams-project", "ams-sub", "ams-token", "remote-endpoint"}
+
+	for _, item := range requiredParams {
+		if flag.Lookup(item).Value.String() == "" {
+			fmt.Printf("required flag not set: -%v \n", item)
+			flag.Usage()
+			os.Exit(1)
+		}
+	}
 
 	// build the client and execute the request
 	transCfg := &http.Transport{
@@ -35,7 +47,7 @@ func main() {
 			log.Printf("ERROR: %v", err)
 		}
 
-		time.Sleep(time.Duration(*pollRate) * time.Second)
+		time.Sleep(time.Duration(*pollRate) * time.Millisecond)
 	}
 
 }
